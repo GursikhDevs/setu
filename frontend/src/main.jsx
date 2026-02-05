91590ccb7c3be5a82ebd83a0c138daa8fa9ec02f
King-Mandeep <mandeeptiwari2006@gmail.com>
2026-02-05 22:41:38 +0530

    fix fileName2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import PageTransition from './components/ui/pageTransition.jsx'
+import PageTransition from './components/ui/PageTransition.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
